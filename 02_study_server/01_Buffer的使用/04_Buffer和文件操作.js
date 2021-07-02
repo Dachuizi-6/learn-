92b29cbc12039173b161c29d4f0bd587685abd51
v_vzhwzhang <v_vzhwzhang@tencent.com>
2021-07-02 10:56:20 +0800

    company

--- 02_study_server/01_Buffer的使用/04_Buffer和文件操作.js
+++ 02_study_server/01_Buffer的使用/04_Buffer和文件操作.js
@@ -18,13 +18,13 @@ const sharp = require("sharp")
 // })
 
 // sharp库的使用
-sharp("./foo.jpg").resize(200, 200).toFile("./bax.png")
+// sharp("./girl.jpg").resize(200, 200).toFile("./bax.png")
 
 sharp("./girl.jpg")
   .resize(300, 300)
   .toBuffer()
   .then((data) => {
-    console.log(data)
+    console.log(data) // 上面转buffer，那你这里肯定是buffer啊
     fs.writeFile("./foy.jpg", data, (err) => {
       console.log(err)
     })
